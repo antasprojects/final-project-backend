c0c0087e713a636c811a8d1d618b6e3f4095b2bd
Antoni Szczekot <asatki@gmail.com>
2024-08-24 15:01:32 +0100

    set up database connection

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const cors = require("cors")
-
 const logger = require('./logger')
 
 const app = express()
@@ -12,5 +11,20 @@ app.use(logger)
 app.get("/", (req, res) => {
     res.send({"final-project-mvc": "healthy"})
 })
+
+
+
+// This block is route for database testing can be deleted later on
+const db = require('./db/connect')
+app.get("/database", async (req, res) => {
+    try {
+        const result = await db.query("SELECT * FROM power_rangers");
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error executing query', error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+//
 
 module.exports = app
