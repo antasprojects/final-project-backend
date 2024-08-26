c753b4b069cfd0ca98ed796fc90f9b28cb45eac3
Antoni Szczekot <asatki@gmail.com>
2024-08-26 15:47:13 +0100

    add logging to s3 bucket

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,5 +1,46 @@
-const logger = (req, res, next) => {
+const AWS = require("aws-sdk")
+
+
+
+const s3 = new AWS.S3()
+const BUCKET_NAME = "s3-bucket-final-project-logs-antoniszczekot"
+
+const logger = async (req, res, next) => {
+    const dateObj = new Date()
+    const formattedDate = dateObj.toISOString().slice(0,10)
+
+    const logEntry = `${new Date().toISOString()} - ${req.ip} - ${req.method}, ${req.originalUrl}\n`;
+    const key = `logs/${formattedDate}-requests.log`
     console.log(req.method, req.originalUrl)
+
+
+    try {
+        let existingLogs = ''
+        try {
+            const data = await s3.getObject({ 
+                Bucket: BUCKET_NAME,
+                Key: key
+            }).promise()
+            existingLogs = data.Body.toString()
+        } catch (err) {
+            if (err.code !== 'NoSuchKey') {
+                throw err
+            }
+        }
+    
+        const updatedLogContent = existingLogs + logEntry
+    
+        await s3.upload({
+            Bucket: BUCKET_NAME,
+            Key: key,
+            Body: updatedLogContent
+        }).promise()
+      } catch (err) {
+        console.log('Failed to upload log entry to S3:', err)
+      }
+    
+
+
     next()
 }
 
