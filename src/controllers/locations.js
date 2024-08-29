73c886f36f2b6de1591f83a94ea9729765391b15
Antoni Szczekot <asatki@gmail.com>
2024-08-29 21:45:20 +0100

    add weather endpoint

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -47,6 +47,51 @@ async function showImages(req, res) {
 
 
 
+async function showWeather(req, res) {
+
+    const id = parseInt(req.params.id);
+    const location = await Location.getOneById(id);
+
+    const lat = location.latitude
+    const lon = location.longitude
+
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
+
+
+    const weather_response = await fetch(url);
+    const data = await weather_response.json()
+
+    const todayDate = new Date().toISOString().split('T')[0];
+    result = []
+
+    for (let i = 0; i <= 32; i++){
+
+      if (data.list[i].dt_txt.slice(-8) == "15:00:00" || data.list[i].dt_txt.slice(0, 10) == todayDate) {
+
+        let object = {
+          date: data.list[i].dt_txt,
+          temperature: data.list[i].main.temp - 273.15,
+          description: data.list[i].weather[0].description,
+          icon_url: `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`
+        }
+        result.push(object)
+      }
+
+    }
+
+
+  if (!weather_response.ok) {
+    throw new Error(`HTTP error! status: ${weather_response.status}`);
+  }
+
+    res.status(200).json(result);
+
+
+  }
+
+
+
+
   module.exports = {
-    show, showImages
+    show, showImages, showWeather
   }
--- src/routers/locations.js
+++ src/routers/locations.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
 
 const locationsController = require("../controllers/locations.js");
-const usersRouter = require("./users.js");
 
 
 const locationsRouter = Router();
 
 locationsRouter.get("/data/:id", locationsController.show);
 locationsRouter.get("/images/:id", locationsController.showImages);
-
+locationsRouter.get("/weather/:id", locationsController.showWeather);
 
 
 module.exports = locationsRouter
