9d9241f7b956a2a2257d4ab3dc40a32205a8d424
Antoni Szczekot <asatki@gmail.com>
2024-08-29 12:25:10 +0100

    add locations/images/:id endpoint

--- src/routers/locations.js
+++ src/routers/locations.js
@@ -7,6 +7,8 @@ const usersRouter = require("./users.js");
 const locationsRouter = Router();
 
 locationsRouter.get("/data/:id", locationsController.show);
+locationsRouter.get("/images/:id", locationsController.showImages);
+
 
 
 module.exports = locationsRouter
