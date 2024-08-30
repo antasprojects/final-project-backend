dc4591a805c645ed3a9bfb6ab7570dd0d4b0c709
Antoni Szczekot <asatki@gmail.com>
2024-08-30 16:19:46 +0100

    add filter path

--- src/routers/locations.js
+++ src/routers/locations.js
@@ -8,6 +8,8 @@ const locationsRouter = Router();
 locationsRouter.get("/data/:id", locationsController.show);
 locationsRouter.get("/images/:id", locationsController.showImages);
 locationsRouter.get("/weather/:id", locationsController.showWeather);
+locationsRouter.post("/filter", locationsController.showFiltered);
+
 
 
 module.exports = locationsRouter
