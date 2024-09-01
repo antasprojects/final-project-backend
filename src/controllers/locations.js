1d823f9619842578a62098693ee362c48236ee26
Liliia Verbenko <liliia.verbenko@lafosseacademy.com>
2024-09-01 09:39:19 +0100

    Added recommndations endpoint

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -1,125 +1,111 @@
 const Location = require("../models/Location");
 
-
-
 async function show(req, res) {
-    try {
-      const id = parseInt(req.params.id);
-      const location = await Location.getOneById(id);
-      res.status(200).json(location);
-    } catch (err) {
-      res.status(404).json({ "error": err.message })
-    }
+  try {
+    const id = parseInt(req.params.id);
+    const location = await Location.getOneById(id);
+    res.status(200).json(location);
+  } catch (err) {
+    res.status(404).json({ "error": err.message });
   }
-
-
+}
 
 async function showImages(req, res) {
   try {
     const id = parseInt(req.params.id);
     const location = await Location.getOneById(id);
-    const name = location.name
-    const perPage = 5
-    
-    console.log(name);
+    const name = location.name;
+    const perPage = 5;
 
-    
+    const tags = `${encodeURIComponent(name.replace(" ", "").toLowerCase())}, nature, landscape`;
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=${tags}&tag_mode=all&safe_search=1&sort=relevance&format=json&nojsoncallback=1&per_page=${perPage}`;
 
-    // const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=${encodeURIComponent(name)}&tag_mode=all&safe_search=1&format=json&nojsoncallback=1&per_page=${perPage}`;
-    // const flickr_response = await fetch(url);
-  
-    const tags = `${encodeURIComponent(name.replace(" ","").toLowerCase())}, nature, landscape`;
-    console.log("tags: ", tags);
-    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&tags=${tags}&tag_mode=all&safe_search=1&sort=relevance&format=json&nojsoncallback=1&per_page=${perPage}`
-    console.log(url);
-    const flickr_response = await fetch(url)
-
+    const flickr_response = await fetch(url);
 
     if (!flickr_response.ok) {
       throw new Error(`Flickr API request failed with status ${flickr_response.status}`);
     }
 
     const data = await flickr_response.json();
-  
-    const photoUrls = data.photos.photo.map(photo => {
+
+    const photoUrls = data.photos.photo.map((photo) => {
       const { id, server, farm, secret } = photo;
-
-      console.log(id, server, farm, secret);
-
       return `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
     });
 
-    console.log(photoUrls);
-
     res.status(200).json(photoUrls);
   } catch (err) {
-    res.status(404).json({ "error": err.message })
+    res.status(404).json({ "error": err.message });
   }
 }
 
-
-
 async function showWeather(req, res) {
-
+  try {
     const id = parseInt(req.params.id);
     const location = await Location.getOneById(id);
 
-    const lat = location.latitude
-    const lon = location.longitude
+    const lat = location.latitude;
+    const lon = location.longitude;
 
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`
-
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`;
 
     const weather_response = await fetch(url);
-    const data = await weather_response.json()
+    const data = await weather_response.json();
 
-    const todayDate = new Date().toISOString().split('T')[0];
-    result = []
+    const todayDate = new Date().toISOString().split("T")[0];
+    const result = [];
 
-    for (let i = 0; i <= 32; i++){
-
-      if (data.list[i].dt_txt.slice(-8) == "15:00:00" || data.list[i].dt_txt.slice(0, 10) == todayDate) {
-
+    for (let i = 0; i <= 32; i++) {
+      if (
+        data.list[i].dt_txt.slice(-8) == "15:00:00" ||
+        data.list[i].dt_txt.slice(0, 10) == todayDate
+      ) {
         let object = {
           date: data.list[i].dt_txt,
           temperature: data.list[i].main.temp - 273.15,
           description: data.list[i].weather[0].description,
-          icon_url: `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`
-        }
-        result.push(object)
+          icon_url: `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`,
+        };
+        result.push(object);
       }
-
     }
 
-
-  if (!weather_response.ok) {
-    throw new Error(`HTTP error! status: ${weather_response.status}`);
-  }
+    if (!weather_response.ok) {
+      throw new Error(`HTTP error! status: ${weather_response.status}`);
+    }
 
     res.status(200).json(result);
-
-
+  } catch (err) {
+    res.status(404).json({ "error": err.message });
   }
+}
 
+async function showFiltered(req, res) {
+  try {
+    const data = req.body;
 
+    const locations = await Location.getFiltered(data.user_location, data.tags, data.filter_distance);
 
-
-  async function showFiltered(req, res) {
-    try {
-      const data = req.body;
-
-      const locations = await Location.getFiltered(data.user_location, data.tags, data.filter_distance)
-
-
-      res.status(200).json(locations);
-    } catch (err) {
-      res.status(404).json({ "error": err.message })
-    }
+    res.status(200).json(locations);
+  } catch (err) {
+    res.status(404).json({ "error": err.message });
   }
+}
 
-
-
-
-  module.exports = {
-    show, showImages, showWeather, showFiltered
+// New controller function for recommendations
+async function showRecommendations(req, res) {
+  try {
+    const recommendations = await Location.getRecommendations();
+    res.status(200).json({ recommendations });
+  } catch (err) {
+    res.status(500).json({ "error": err.message });
   }
+}
+
+module.exports = {
+  show,
+  showImages,
+  showWeather,
+  showFiltered,
+  showRecommendations
+};
--- src/models/Location.js
+++ src/models/Location.js
@@ -1,69 +1,67 @@
 const db = require("../db/connect");
-const { getBoundaries } = require("./helpers.js")
-
-
-
-
+const { getBoundaries } = require("./helpers.js");
 
 class Location {
-      constructor({place_id, name, location_type, description, latitude, longitude, rating, address, phone_number, website_url, image_url, opening_hours, tags }) {
-        this.place_id = place_id;
-        this.name = name;
-        this.location_type = location_type;
-        this.description = description;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.rating = rating;
-        this.address = address;
-        this.phone_number = phone_number;
-        this.website_url = website_url;
-        this.image_url = image_url;
-        this.opening_hours = opening_hours;
-        this.tags = tags
-      }
-      
+  constructor({
+    place_id,
+    name,
+    location_type,
+    description,
+    latitude,
+    longitude,
+    rating,
+    address,
+    phone_number,
+    website_url,
+    image_url,
+    opening_hours,
+    tags,
+  }) {
+    this.place_id = place_id;
+    this.name = name;
+    this.location_type = location_type;
+    this.description = description;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.rating = rating;
+    this.address = address;
+    this.phone_number = phone_number;
+    this.website_url = website_url;
+    this.image_url = image_url;
+    this.opening_hours = opening_hours;
+    this.tags = tags;
+  }
 
+  static async getOneById(id) {
+    const response = await db.query("SELECT * FROM green_places WHERE place_id = $1;", [id]);
 
-    static async getOneById(id) {
-      const response = await db.query("SELECT * FROM green_places WHERE place_id = $1;", [id]);
-
-
-      if (response.rows.length != 1) {
-        throw new Error("Unable to locate location.")
-      }
-
-      const response_tags = await db.query("SELECT tag_name FROM tags WHERE place_id = $1;", [id]);
-
-
-      if (response_tags.rows.length == 0) {
-        throw new Error("Unable to locate tags.")
-      }
-
-      const tag_names = response_tags.rows.map(tag => tag.tag_name);
-
-      response.rows[0].tags = tag_names
-
-  
-      return new Location(response.rows[0]);
+    if (response.rows.length != 1) {
+      throw new Error("Unable to locate location.");
     }
 
+    const response_tags = await db.query("SELECT tag_name FROM tags WHERE place_id = $1;", [id]);
 
+    if (response_tags.rows.length == 0) {
+      throw new Error("Unable to locate tags.");
+    }
 
+    const tag_names = response_tags.rows.map((tag) => tag.tag_name);
 
+    response.rows[0].tags = tag_names;
 
+    return new Location(response.rows[0]);
+  }
 
-      static async getFiltered(user_location, tags, filter_distance) {
+  static async getFiltered(user_location, tags, filter_distance) {
+    const { latitude, longitude } = user_location;
+    const activeTags = Object.keys(tags).filter((tag) => tags[tag]);
+    const { latMin, latMax, lonMin, lonMax } = getBoundaries(latitude, longitude, filter_distance);
 
-      const { latitude, longitude } = user_location
-      const activeTags = Object.keys(tags).filter(tag => tags[tag]);
-      const {latMin, latMax, lonMin, lonMax} = getBoundaries(latitude, longitude, filter_distance)
+    let query;
+    let queryParams = [latMin, latMax, lonMin, lonMax];
 
-      let query;
-      let queryParams = [latMin, latMax, lonMin, lonMax];
-  
-      if (activeTags.length > 0) {
-        
-        query = `
+    if (activeTags.length > 0) {
+      query = `
         SELECT DISTINCT p.*
         FROM green_places p
         JOIN tags t ON p.place_id = t.place_id
@@ -71,22 +69,48 @@ class Location {
         AND p.longitude BETWEEN $3 AND $4
         AND t.tag_name = ANY($5::text[]);`;
 
-        queryParams.push(activeTags);
-      } else {
-        
-        query = `
-          SELECT DISTINCT p.* 
-          FROM green_places p
-          WHERE p.latitude BETWEEN $1 AND $2 
-          AND p.longitude BETWEEN $3 AND $4;`;
-      }
+      queryParams.push(activeTags);
+    } else {
+      query = `
+        SELECT DISTINCT p.* 
+        FROM green_places p
+        WHERE p.latitude BETWEEN $1 AND $2 
+        AND p.longitude BETWEEN $3 AND $4;`;
+    }
 
-  
     const result = await db.query(query, queryParams);
 
-      return result.rows;
-    }
-}
+    return result.rows;
+  }
 
+  // New function to get recommendations
+  static async getRecommendations() {
+    try {
+      const response = await db.query(`
+        SELECT name, description, address, phone_number 
+        FROM green_places;
+      `);
+
+      if (response.rows.length === 0) {
+        return [];
+      }
+
+      const recommendations = response.rows.map((location) => {
+        const { name, description, address, phone_number } = location;
+
+        return {
+          name,
+          description: description || 'No description available',
+          address: address || 'No address available',
+          phone_number: phone_number || 'No phone number available',
+        };
+      });
+
+      return recommendations;
+    } catch (err) {
+      throw new Error("Error retrieving location recommendations: " + err.message);
+    }
+  }
+}
 
 module.exports = Location;
