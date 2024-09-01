a2ddfa2366fe1f05b03f7c9bf7e667ec229216c6
Antoni Szczekot <asatki@gmail.com>
2024-09-01 18:45:40 +0100

    fix bugs from merge

--- src/models/Location.js
+++ src/models/Location.js
@@ -2,57 +2,43 @@ const db = require("../db/connect");
 const { getBoundaries } = require("./helpers.js");
 
 class Location {
-      constructor({place_id, name, location_type, description, latitude, longitude, rating, address, phone_number, website_url, image_url, opening_hours, tags }) {
-        this.place_id = place_id;
-        this.name = name;
-        this.location_type = location_type;
-        this.description = description;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.rating = rating;
-        this.address = address;
-        this.image_url = image_url;
-        this.tag_id = tag_id
-      }
+  constructor({place_id, name, location_type, description, latitude, longitude, rating, address, image_url, tag_id }) {
+    this.place_id = place_id;
+    this.name = name;
+    this.location_type = location_type;
+    this.description = description;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.rating = rating;
+    this.address = address;
+    this.image_url = image_url;
+    this.tag_id = tag_id
+  }
       
+  static async getOneById(id) {
+    const response = await db.query("SELECT * FROM green_places WHERE place_id = $1;", [id]);
 
 
-    static async getOneById(id) {
-      const response = await db.query("SELECT * FROM green_places WHERE place_id = $1;", [id]);
-
-
-      if (response.rows.length != 1) {
-        throw new Error("Unable to locate location.")
-      }
-
-      const response_tag = await db.query("SELECT tag_name FROM tags WHERE tag_id = $1;", [response.rows[0].tag_id]);
-
-
-      if (response_tag.rows.length == 0) {
-        throw new Error("Unable to locate tags.")
-      }
-
-      return response.rows[0];
+    if (response.rows.length != 1) {
+      throw new Error("Unable to locate location.")
     }
 
-    const response_tags = await db.query("SELECT tag_name FROM tags WHERE place_id = $1;", [id]);
+    const response_tag = await db.query("SELECT tag_name FROM tags WHERE tag_id = $1;", [response.rows[0].tag_id]);
 
-    if (response_tags.rows.length == 0) {
-      throw new Error("Unable to locate tags.");
+
+    if (response_tag.rows.length == 0) {
+      throw new Error("Unable to locate tags.")
     }
 
-    const tag_names = response_tags.rows.map((tag) => tag.tag_name);
-
-    response.rows[0].tags = tag_names;
-
-    return new Location(response.rows[0]);
+    return response.rows[0];
   }
 
+
   static async getFiltered(user_location, tags, filter_distance) {
+
     const { latitude, longitude } = user_location;
     const activeTags = Object.keys(tags).filter((tag) => tags[tag]);
     const { latMin, latMax, lonMin, lonMax } = getBoundaries(latitude, longitude, filter_distance);
-
     let query;
     let queryParams = [latMin, latMax, lonMin, lonMax];
 
@@ -64,8 +50,8 @@ class Location {
         WHERE p.latitude BETWEEN $1 AND $2
         AND p.longitude BETWEEN $3 AND $4
         AND t.tag_name = ANY($5::text[]);`;
-
       queryParams.push(activeTags);
+
     } else {
       query = `
         SELECT DISTINCT p.* 
