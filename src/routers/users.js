d07e65e576ec2a60e1fb8228ee1fa1501adb1c8c
Liliia Verbenko <liliia.verbenko@lafosseacademy.com>
2024-09-03 13:41:17 +0100

    Recommended places endpoint added

--- src/routers/users.js
+++ src/routers/users.js
@@ -9,12 +9,8 @@ usersRouter.post("/register", usersControllers.register);
 usersRouter.post("/login", usersControllers.login);
 usersRouter.get("/validate-token", usersControllers.tokenValidation);
 usersRouter.get("/stats", authenticator, usersControllers.showStats);
-
-// New routes for saving and retrieving locations
 usersRouter.post("/save", authenticator, usersControllers.saveLocation);
 usersRouter.get("/retrieve", authenticator, usersControllers.getSavedLocations);
-
-// New routes for recommending locations and retrieving recommendations
 usersRouter.post("/recommend", authenticator, usersControllers.recommendLocation);
 usersRouter.get("/recommendations", authenticator, usersControllers.getRecommendations);
 
